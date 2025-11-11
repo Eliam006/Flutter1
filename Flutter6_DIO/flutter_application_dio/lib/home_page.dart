2a7fc77971d8e79a9603b375ec65fc43e2a89161
Eliam006 <133276178+Eliam006@users.noreply.github.com>
2025-11-10 22:56:44 -0600

    Ejercicio Dio

---
+++ Flutter6_DIO/flutter_application_dio/lib/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_dio/api_service.dart';
+
+class HomePage extends StatefulWidget{
+  const HomePage({super.key});
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+  
+}
+
+class _HomePageState extends State<HomePage>{
+  final ApiService _api = ApiService();
+  List<dynamic> posts = [];
+  bool loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    //Aqui se cargan los datos
+    loadData();
+  }
+
+  Future<void> loadData() async{
+    final result = await _api.getPosts();
+    setState(() {
+      posts = result;
+      loading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( title: Text('Demo con DIO Eliam'),),
+      body: loading
+            ? Center(child: CircularProgressIndicator(),)
+            :ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (context, index){
+                final item = posts[index];
+                return ListTile(
+                  title: Text(item['title']),
+                  subtitle: Text(item['body']),
+                );
+              },
+            )
+    );
+  }
+  
+}
